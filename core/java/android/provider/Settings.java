0f273643528e82ffe8f0edd78251fd92b3df4e32
alexxxdev <alexxxdev@yandex.ru>
2019-10-17 22:24:24 +0100

    Extended Audio Panel [1/2]
    Change-Id: Ieb85137ee73f7ed032b9bbdd4087be2c72a4d1f5
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6172,6 +6172,24 @@ public final class Settings {
         private static final Validator HEADS_UP_TIMEOUT_VALIDATOR =
               new SettingsValidators.InclusiveIntegerRangeValidator(0, 10000);
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
+
         /**
          * Whether to show heads up only for dialer and sms apps
          * @hide
