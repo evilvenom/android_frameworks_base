fa84d8e6d81177a45303749f07f0130374f40df1
00day0 <therandomuser11@gmail.com>
2020-04-07 02:05:34 +0530

    base: Add toggle for FOD animation [1/2]

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -96,6 +96,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     private Timer mBurnInProtectionTimer;
 
+    private FODAnimation mFODAnimation;
+    private boolean mIsRecognizingAnimEnabled;
+
     private FodScreenOffHandler mFodScreenOffHandler;
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
@@ -133,6 +136,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         public void onKeyguardVisibilityChanged(boolean showing) {
             mIsKeyguard = showing;
             updatePosition();
+            updateSettings();
+
+            if (mFODAnimation != null) {
+                mFODAnimation.setAnimationKeyguard(showing);
+            }
         }
 
         @Override
@@ -292,6 +300,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         mWindowManager.addView(this, mParams);
 
+        updateSettings();
         updatePosition();
         hide();
 
@@ -322,6 +331,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN && newIsInside) {
             showCircle();
+            if (mIsRecognizingAnimEnabled) {
+                mHandler.post(() -> mFODAnimation.showFODanimation());
+            }
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             hideCircle();
@@ -335,6 +347,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
+        updateSettings();
         updatePosition();
     }
 
@@ -543,6 +556,10 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         }
 
         return false;
+
+    private void updateSettings() {
+        mIsRecognizingAnimEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_RECOGNIZING_ANIMATION, 1) != 0;
     }
 
     private class BurnInProtectionTask extends TimerTask {
